be1b34f9f7b5b068fbcf4d32c09c16b1923cf82e
dseibert <dan.seibert@tritonsvc.com>
2016-02-11 16:02:12 -0800

    added spring-data-rest layer.  Lets us remove SpasApi class.

--- src/main/java/com/bwg/iot/SpaRepository.java
+++ src/main/java/com/bwg/iot/SpaRepository.java
@@ -1,10 +1,18 @@
 package com.bwg.iot;
 
 import com.bwg.iot.model.Spa;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 /**
  * Created by triton on 2/10/16.
  */
 public interface SpaRepository extends MongoRepository<Spa, String> {
+
+    public List<Spa> findByDealerId(@Param("dealerId") String dealerId);
+
 }
