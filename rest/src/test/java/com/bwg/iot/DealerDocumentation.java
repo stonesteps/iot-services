66dd2b247bc7499aadf778a9cb641162b8ba9766
dseibert <dan.seibert@tritonsvc.com>
2016-02-22 17:33:15 -0800

    Added Dealer Entity CRUD service.   Includes model object, repository, unit tests, api docs

---
+++ rest/src/test/java/com/bwg/iot/DealerDocumentation.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2014-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bwg.iot;
+
+import com.bwg.iot.model.Address;
+import com.bwg.iot.model.Alert;
+import com.bwg.iot.model.Dealer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = IotServicesApplication.class)
+@WebAppConfiguration
+public final class DealerDocumentation extends ModelTestBase{
+
+	@Rule
+	public final JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation("target/generated-snippets");
+
+	@Autowired
+	private AddressRepository addressRepository;
+
+	@Autowired
+	private DealerRepository dealerRepository;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Autowired
+	private WebApplicationContext context;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setUp() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+				.apply(documentationConfiguration(this.restDocumentation)).build();
+	}
+
+	@Test
+	public void dealerListExample() throws Exception {
+		this.dealerRepository.deleteAll();
+
+
+		this.mockMvc.perform(get("/dealers")).andExpect(status().isOk())
+				.andDo(document("dealers-list-example",
+						responseFields(
+								fieldWithPath("_embedded.dealers")
+										.description("An array of <<resources-dealer, Dealer resources>>"),
+						fieldWithPath("_links").description("<<resources-dealerslist-links,Links>> to other resources"),
+						fieldWithPath("page").description("Page information"))));
+	}
+
+	@Test
+	public void dealerCreateExample() throws Exception {
+		this.dealerRepository.deleteAll();
+		this.addressRepository.deleteAll();
+
+		Address address = createAddress();
+
+        String now = LocalDateTime.now().toString();
+		final Map<String, Object> dealer = new HashMap<>();
+		dealer.put("name", "South Coast Spas");
+		dealer.put("address", address);
+
+		this.mockMvc
+				.perform(post("/dealers").contentType(MediaTypes.HAL_JSON)
+						.content(this.objectMapper.writeValueAsString(dealer)))
+				.andExpect(status().isCreated())
+				.andDo(document("dealer-create-example",
+						requestFields(fieldWithPath("name").description("Name of the dealer"),
+//								fieldWithPath("id").description("Unique identifier for the dealer"),
+								fieldWithPath("address").description("The address of the dealer"))));
+	}
+
+	@Test
+	public void dealerUpdateExample() throws Exception {
+		this.dealerRepository.deleteAll();
+        this.addressRepository.deleteAll();
+
+        Address address = createAddress();
+		Dealer dealer = createDealer("Backyard Beach", address);
+
+		final Map<String, String> dealerUpdate = new HashMap<>();
+		dealerUpdate.put("name", "Spalicious");
+
+		this.mockMvc
+				.perform(patch("/dealers/{0}", dealer.getId()).contentType(MediaTypes.HAL_JSON)
+						.content(this.objectMapper.writeValueAsString(dealerUpdate)))
+				.andExpect(status().is2xxSuccessful())
+				.andDo(document("dealer-update-example",
+						requestFields(fieldWithPath("name").description("Business name of the dealer"))));
+	}
+
+	@Test
+	public void dealerGetExample() throws Exception {
+        this.dealerRepository.deleteAll();
+        this.addressRepository.deleteAll();
+
+        Address address = createAddress();
+        Dealer dealer = createDealer("Backyard Beach", address);
+
+
+        this.mockMvc.perform(get("/dealers/{0}", dealer.getId())).andExpect(status().isOk())
+				.andExpect(jsonPath("name", is(dealer.getName())))
+//				.andExpect(jsonPath("address", is(dealer.getAddress())))
+				.andDo(document("dealer-get-example",
+						links(linkWithRel("self").description("This <<resources-dealer,dealer>>"),
+								linkWithRel("dealer").description("This <<resources-dealer,dealer>>")),
+						responseFields(fieldWithPath("name").description("The name of the dealer"),
+								fieldWithPath("address").description("Contact info for the dealer (yellow, red)"),
+                                fieldWithPath("_links")
+										.description("<<resources-dealer-links,Links>> to other resources"))));
+	}
+}
---
+++ spa-model/src/main/java/com/bwg/iot/model/Dealer.java
@@ -0,0 +1,68 @@
+package com.bwg.iot.model;
+
+import org.springframework.data.annotation.Id;
+
+import java.util.Objects;
+
+
+public class Dealer {
+
+    @Id
+    private String id;
+    private String name;
+    private Address address;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      }
+      if (o == null || getClass() != o.getClass()) {
+        return false;
+      }
+      Dealer dealer = (Dealer) o;
+      return Objects.equals(id, dealer.id) &&
+          Objects.equals(name, dealer.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString()  {
+        StringBuilder sb = new StringBuilder();
+        sb.append("class Dealer {\n");
+
+        sb.append("  id: ").append(id).append("\n");
+        sb.append("  name: ").append(name).append("\n");
+        sb.append("  address: ").append(address.toString()).append("\n");
+        sb.append("}\n");
+        return sb.toString();
+    }
+}
