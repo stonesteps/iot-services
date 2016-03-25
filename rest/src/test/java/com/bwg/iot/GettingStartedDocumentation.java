aa2ab7dbee7929b00f92de4a5fdd49d83756ad94
http://dan.seibert@tritonsvc.com@iotdev05.bi.local <dan.seibert@tritonsvc.com>
2016-03-25 16:42:01 -0700

    getting started documentation for terms and agreements

--- rest/src/test/java/com/bwg/iot/GettingStartedDocumentation.java
+++ rest/src/test/java/com/bwg/iot/GettingStartedDocumentation.java
@@ -31,8 +31,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import com.bwg.iot.model.Address;
+import com.bwg.iot.model.TacUserAgreement;
+import com.bwg.iot.model.TermsAndConditions;
+import com.bwg.iot.model.User;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -45,6 +50,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -55,7 +61,7 @@ import com.jayway.jsonpath.JsonPath;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = IotServicesApplication.class)
 @WebAppConfiguration
-public class GettingStartedDocumentation {
+public class GettingStartedDocumentation extends ModelTestBase {
 	
 	@Rule
 	public final JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation("target/generated-snippets");
@@ -83,6 +89,25 @@ public class GettingStartedDocumentation {
 			.andExpect(jsonPath("_links.spas", is(notNullValue())));
 	}
 
+	@Test
+    public void termsAndConditions() throws JsonProcessingException, Exception {
+        clearAllData();
+        List<Address> addresses = createAddresses(2);
+        User owner1 = createUser("user0001", "braitt", "Bonnie", "Raitt", "dealer0001", "oem04", addresses.get(0), Arrays.asList("OWNER"));
+        User owner2 = createUser("user0002", "stones", "Keith", "Richards", "dealer0001", "oem04", addresses.get(1), Arrays.asList("OWNER"));
+
+        TermsAndConditions tac1 = createTermsAndAgreement("1.0", "The world is mine");
+        TacUserAgreement agreement1 = createAgreement(owner1.get_id(), tac1.getVersion());
+
+        login(owner1);
+
+        findAgreement(owner1);
+        dontFindAgreement(owner2);
+        findMostRecentTermsAndConditions();
+        agreeToTerms(owner2, tac1);
+
+	}
+
 //	@Test
 //	public void creatingANote() throws JsonProcessingException, Exception {
 //		String noteLocation = createNote();
@@ -99,93 +124,121 @@ public class GettingStartedDocumentation {
 //		getTags(getLink(note, "tags"));
 //	}
 
-	String createNote() throws Exception {
-		Map<String, String> note = new HashMap<String, String>();
-		note.put("title", "Note creation with cURL");
-		note.put("body", "An example of how to create a note using cURL");
+	private ResultActions login(User owner) throws Exception {
+		Map<String, String> login = new HashMap<String, String>();
+		login.put("username", owner.getUsername());
+		login.put("password", "*******");
 
-		String noteLocation = this.mockMvc
+		ResultActions loginLocation = this.mockMvc
 				.perform(
-						post("/notes").contentType(MediaTypes.HAL_JSON).content(
-								objectMapper.writeValueAsString(note)))
-				.andExpect(status().isCreated())
-				.andExpect(header().string("Location", notNullValue()))
-				.andReturn().getResponse().getHeader("Location");
-		return noteLocation;
+						post("/auth/login").contentType(MediaTypes.HAL_JSON).content(
+								objectMapper.writeValueAsString(login)))
+				.andExpect(status().is2xxSuccessful());
+		return loginLocation;
 	}
 
-	MvcResult getNote(String noteLocation) throws Exception {
-		return this.mockMvc.perform(get(noteLocation))
-				.andExpect(status().isOk())
-				.andExpect(jsonPath("title", is(notNullValue())))
-				.andExpect(jsonPath("body", is(notNullValue())))
-				.andExpect(jsonPath("_links.tags", is(notNullValue())))
-				.andReturn();
-	}
+    private ResultActions findAgreement(User user) throws Exception {
+        return this.mockMvc.perform(
+                        get("/tac/search/findCurrentUserAgreement?userId="+user.get_id()))
+                .andExpect(status().is2xxSuccessful());
+    }
 
-	String createTag() throws Exception, JsonProcessingException {
-		Map<String, String> tag = new HashMap<String, String>();
-		tag.put("name", "getting-started");
+    private ResultActions dontFindAgreement(User user) throws Exception {
+        return this.mockMvc.perform(
+                get("/tac/search/findCurrentUserAgreement?userId="+user.get_id()))
+                .andExpect(status().is4xxClientError());
+    }
 
-		String tagLocation = this.mockMvc
-				.perform(
-						post("/tags").contentType(MediaTypes.HAL_JSON).content(
-								objectMapper.writeValueAsString(tag)))
-				.andExpect(status().isCreated())
-				.andExpect(header().string("Location", notNullValue()))
-				.andReturn().getResponse().getHeader("Location");
-		return tagLocation;
-	}
+    private ResultActions findMostRecentTermsAndConditions() throws Exception {
+        return this.mockMvc.perform(
+                get("/tac/search/findMostRecent"))
+                .andExpect(status().isOk());
+    }
+    private ResultActions agreeToTerms(User user, TermsAndConditions terms) throws Exception {
+        Map<String, String> agree = new HashMap<String, String>();
+        agree.put("userId", user.get_id());
+        agree.put("version", terms.getVersion());
 
-	void getTag(String tagLocation) throws Exception {
-		this.mockMvc.perform(get(tagLocation)).andExpect(status().isOk())
-			.andExpect(jsonPath("name", is(notNullValue())))
-			.andExpect(jsonPath("_links.notes", is(notNullValue())));
-	}
+        ResultActions agreement = this.mockMvc
+                .perform(
+                        post("/tac/agree").contentType(MediaTypes.HAL_JSON).content(
+                                objectMapper.writeValueAsString(agree)))
+                .andExpect(status().is2xxSuccessful());
+        return agreement;
+    }
 
-	String createTaggedNote(String tag) throws Exception {
-		Map<String, Object> note = new HashMap<String, Object>();
-		note.put("title", "Tagged note creation with cURL");
-		note.put("body", "An example of how to create a tagged note using cURL");
-		note.put("tags", Arrays.asList(tag));
-
-		String noteLocation = this.mockMvc
-				.perform(
-						post("/notes").contentType(MediaTypes.HAL_JSON).content(
-								objectMapper.writeValueAsString(note)))
-				.andExpect(status().isCreated())
-				.andExpect(header().string("Location", notNullValue()))
-				.andReturn().getResponse().getHeader("Location");
-		return noteLocation;
-	}
-
-	void getTags(String noteTagsLocation) throws Exception {
-		this.mockMvc.perform(get(noteTagsLocation))
-			.andExpect(status().isOk())
-			.andExpect(jsonPath("_embedded.tags", hasSize(1)));
-	}
-
-	void tagExistingNote(String noteLocation, String tagLocation) throws Exception {
-		Map<String, Object> update = new HashMap<String, Object>();
-		update.put("tags", Arrays.asList(tagLocation));
-
-		this.mockMvc.perform(
-				patch(noteLocation).contentType(MediaTypes.HAL_JSON).content(
-						objectMapper.writeValueAsString(update)))
-				.andExpect(status().isNoContent());
-	}
-
-	MvcResult getTaggedExistingNote(String noteLocation) throws Exception {
-		return this.mockMvc.perform(get(noteLocation))
-			.andExpect(status().isOk())
-			.andReturn();
-	}
-
-	void getTagsForExistingNote(String noteTagsLocation) throws Exception {
-		this.mockMvc.perform(get(noteTagsLocation))
-			.andExpect(status().isOk())
-			.andExpect(jsonPath("_embedded.tags", hasSize(1)));
-	}
+//	MvcResult getNote(String noteLocation) throws Exception {
+//		return this.mockMvc.perform(get(noteLocation))
+//				.andExpect(status().isOk())
+//				.andExpect(jsonPath("title", is(notNullValue())))
+//				.andExpect(jsonPath("body", is(notNullValue())))
+//				.andExpect(jsonPath("_links.tags", is(notNullValue())))
+//				.andReturn();
+//	}
+//
+//	String createTag() throws Exception, JsonProcessingException {
+//		Map<String, String> tag = new HashMap<String, String>();
+//		tag.put("name", "getting-started");
+//
+//		String tagLocation = this.mockMvc
+//				.perform(
+//						post("/tags").contentType(MediaTypes.HAL_JSON).content(
+//								objectMapper.writeValueAsString(tag)))
+//				.andExpect(status().isCreated())
+//				.andExpect(header().string("Location", notNullValue()))
+//				.andReturn().getResponse().getHeader("Location");
+//		return tagLocation;
+//	}
+//
+//	void getTag(String tagLocation) throws Exception {
+//		this.mockMvc.perform(get(tagLocation)).andExpect(status().isOk())
+//			.andExpect(jsonPath("name", is(notNullValue())))
+//			.andExpect(jsonPath("_links.notes", is(notNullValue())));
+//	}
+//
+//	String createTaggedNote(String tag) throws Exception {
+//		Map<String, Object> note = new HashMap<String, Object>();
+//		note.put("title", "Tagged note creation with cURL");
+//		note.put("body", "An example of how to create a tagged note using cURL");
+//		note.put("tags", Arrays.asList(tag));
+//
+//		String noteLocation = this.mockMvc
+//				.perform(
+//						post("/notes").contentType(MediaTypes.HAL_JSON).content(
+//								objectMapper.writeValueAsString(note)))
+//				.andExpect(status().isCreated())
+//				.andExpect(header().string("Location", notNullValue()))
+//				.andReturn().getResponse().getHeader("Location");
+//		return noteLocation;
+//	}
+//
+//	void getTags(String noteTagsLocation) throws Exception {
+//		this.mockMvc.perform(get(noteTagsLocation))
+//			.andExpect(status().isOk())
+//			.andExpect(jsonPath("_embedded.tags", hasSize(1)));
+//	}
+//
+//	void tagExistingNote(String noteLocation, String tagLocation) throws Exception {
+//		Map<String, Object> update = new HashMap<String, Object>();
+//		update.put("tags", Arrays.asList(tagLocation));
+//
+//		this.mockMvc.perform(
+//				patch(noteLocation).contentType(MediaTypes.HAL_JSON).content(
+//						objectMapper.writeValueAsString(update)))
+//				.andExpect(status().isNoContent());
+//	}
+//
+//	MvcResult getTaggedExistingNote(String noteLocation) throws Exception {
+//		return this.mockMvc.perform(get(noteLocation))
+//			.andExpect(status().isOk())
+//			.andReturn();
+//	}
+//
+//	void getTagsForExistingNote(String noteTagsLocation) throws Exception {
+//		this.mockMvc.perform(get(noteTagsLocation))
+//			.andExpect(status().isOk())
+//			.andExpect(jsonPath("_embedded.tags", hasSize(1)));
+//	}
 
 	private String getLink(MvcResult result, String rel)
 			throws UnsupportedEncodingException {
