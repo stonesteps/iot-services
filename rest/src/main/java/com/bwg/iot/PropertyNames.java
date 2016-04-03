84e330d02d807c81bc9d8aca6cd770be2a839213
http://dan.seibert@tritonsvc.com@iotdev05.bi.local <dan.seibert@tritonsvc.com>
2016-04-02 23:54:52 -0700

    remote user header

---
+++ rest/src/main/java/com/bwg/iot/PropertyNames.java
@@ -0,0 +1,9 @@
+package com.bwg.iot;
+
+/**
+ * Created by triton on 4/2/16.
+ */
+public final class PropertyNames {
+    public static final String USER_TOKEN_HEADER = "user.token.header";
+    public static final String TOKEN_ENDPOINT = "token.endpoint";
+}
