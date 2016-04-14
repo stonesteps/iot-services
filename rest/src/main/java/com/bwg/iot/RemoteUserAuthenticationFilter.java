64deae99b6630f349bbbc51f2fc561218e485ec5
http://dan.seibert@tritonsvc.com@iotdev05.bi.local <dan.seibert@tritonsvc.com>
2016-04-14 14:39:34 -0700

    qa security debug

--- rest/src/main/java/com/bwg/iot/RemoteUserAuthenticationFilter.java
+++ rest/src/main/java/com/bwg/iot/RemoteUserAuthenticationFilter.java
@@ -1,6 +1,8 @@
 package com.bwg.iot;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -15,6 +17,7 @@ import java.io.IOException;
  * Created by triton on 4/4/16.
  */
 public class RemoteUserAuthenticationFilter extends OncePerRequestFilter{
+    private final Logger log = LoggerFactory.getLogger(RemoteUserAuthenticationFilter.class);
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
@@ -22,7 +25,7 @@ public class RemoteUserAuthenticationFilter extends OncePerRequestFilter{
             throws ServletException, IOException {
 
         String authUser = request.getHeader("remote_user");
-
+        log.info("RemoteUserAuthFiler   remote_user: " + authUser);
         if (StringUtils.isNotEmpty(authUser)) {
             Authentication auth = new RemoteUserToken(authUser);
             SecurityContextHolder.getContext().setAuthentication(auth);
