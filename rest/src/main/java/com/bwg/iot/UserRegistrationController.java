d8a63c3d780725cd2b15f83b0c57b6e593e364bc
Matteo Minnai <m.minnai@entando.com>
2016-06-02 19:04:47 +0200

    Improved user creation

--- rest/src/main/java/com/bwg/iot/UserRegistrationController.java
+++ rest/src/main/java/com/bwg/iot/UserRegistrationController.java
@@ -5,19 +5,31 @@
 */
 package com.bwg.iot;
 
+import com.bwg.iot.model.GluuUser;
 import com.bwg.iot.model.User;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.logging.Level;
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.hateoas.EntityLinks;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  *
@@ -29,38 +41,63 @@ public class UserRegistrationController {
   
   private static final Logger log = LoggerFactory.getLogger(UserRegistrationController.class);
   
+  private final UserRepository userRepository;
+  
+  private String scimUser;
+  private String scimPassword;
+  private String scimUrl;
+  
   @Autowired
   UserRegistrationHelper gluuHelper;
   
+  @Autowired
+  MongoOperations mongoOps;
+  
+  @Autowired
+  ObjectMapper objectMapper;
+  
+  @Autowired
+  Environment environment;
+  
+  @Autowired
+  public UserRegistrationController(UserRepository repo) {
+    userRepository = repo;
+  }
+  
+  @PostConstruct
+  public void initialize() {
+    scimUser = environment.getProperty(PropertyNames.SCIM_USER);
+    scimPassword = environment.getProperty(PropertyNames.SCIM_PWD);
+    scimUrl = environment.getProperty(PropertyNames.SCIM_URL);
+    log.info("user regsitration controller initialized succesfully");
+  }
   
   @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-  public ResponseEntity<?> createUser(@RequestBody HashMap<String, String> request) {
-    
-    User user = new User();
+  public ResponseEntity<?> createUser(@RequestBody com.bwg.iot.model.User user) {
     try {
-      String firstName = request.get("firstName");
-      String lastName = request.get("lastName");
-      String username = request.get("id");
-      String email = request.get("email");
       
-      
-      user.setUsername(username);
-      user.setFirstName(firstName);
-      user.setLastName(lastName);
-      user.setEmail(email);
-      
-      if (StringUtils.isNotBlank(username)) {
+      if (StringUtils.isNotBlank(user.getUsername())) {
         JsonNode json = gluuHelper.createUser(user);
-        log.info("New user added with id " + json.get("id").textValue());
+        log.info("New user added with Gluu id " + json.get("id").textValue());
       } else {
         log.error("username is undefined, aborting user creation");
       }
     } catch (Throwable t) {
       t.printStackTrace();
     }
+    // save
+    userRepository.save(user);
     
     ResponseEntity<?> response = new ResponseEntity<User>(user, HttpStatus.OK);
     return response;
   }
+  
+  /*
+  @RequestMapping("/foo")
+  void handleFoo(HttpServletResponse response) {
+    response.sendRedirect("some-url");
+  }
+  */
+ 
   
 }
