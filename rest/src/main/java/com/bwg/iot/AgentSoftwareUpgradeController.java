2ba4d3c32368e10cfa9d6eccec86ae285a8728ce
Marek Kwiecien <marek.kwiecien.ext@optile.net>
2016-06-21 18:48:05 +0200

    update - returning latest software package if requested version is not found

--- rest/src/main/java/com/bwg/iot/AgentSoftwareUpgradeController.java
+++ rest/src/main/java/com/bwg/iot/AgentSoftwareUpgradeController.java
@@ -9,9 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileFilter;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * Holds info about latest gateway agent software build and is able to return software package if requesting party
@@ -21,6 +26,19 @@ import java.text.MessageFormat;
 @RequestMapping("/sw_upgrade")
 public class AgentSoftwareUpgradeController {
 
+    private static final FileFilter TAR_GZ_ONLY = new FileFilter() {
+        @Override
+        public boolean accept(final File pathname) {
+            return pathname.isFile() && pathname.getName().toLowerCase().endsWith(".tar.gz");
+        }
+    };
+
+    private static final Comparator<File> LAST_MODIFIED_COMPARATOR = new Comparator<File>() {
+        @Override
+        public int compare(final File f1, final File f2) {
+            return Long.compare(f1.lastModified(), f2.lastModified());
+        }
+    };
 
     @Value("${agentSoftwareUpgradePackageFolder:./sw_upgrade}")
     private String agentSoftwareUpgradePackageFolder;
@@ -33,13 +51,14 @@ public class AgentSoftwareUpgradeController {
             @RequestParam("currentBuildNumber") final String buildNumber, final HttpServletResponse response) {
 
         final File softwareUpgradePackageFile = getSoftwareUpgradePackageFile(buildNumber);
-        if (!softwareUpgradePackageFile.exists()) {
+        final File latestSoftwareUpgradeFile = getLatestSoftwareUpgradePackageFile();
+        if (softwareUpgradePackageFile.exists() || latestSoftwareUpgradeFile == null) {
             response.setStatus(HttpServletResponse.SC_NO_CONTENT);
         } else {
-            try (final InputStream is = new FileInputStream(softwareUpgradePackageFile)) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            try (final InputStream is = new FileInputStream(latestSoftwareUpgradeFile)) {
                 IOUtils.copy(is, response.getOutputStream());
                 response.flushBuffer();
-
             } catch (final Exception e) {
                 throw new RuntimeException("Error while returning software upgrade package");
             }
@@ -55,5 +74,26 @@ public class AgentSoftwareUpgradeController {
         final String fileName = MessageFormat.format(agentSoftwareUpgradePackageFileNameTemplate, version);
         sb.append(fileName);
         return new File(sb.toString());
+    }
+
+    private File getLatestSoftwareUpgradePackageFile() {
+        final File softwareUpgradeFolder = new File(agentSoftwareUpgradePackageFolder);
+        if (softwareUpgradeFolder.exists() && softwareUpgradeFolder.isDirectory()) {
+            final File[] contents = softwareUpgradeFolder.listFiles(TAR_GZ_ONLY);
+            return latest(contents);
+        }
+        return null;
+    }
+
+    private File latest(File[] contents) {
+        final File latest = null;
+
+        if (contents != null && contents.length > 0) {
+            final List<File> files = Arrays.asList(contents);
+            Collections.sort(files, LAST_MODIFIED_COMPARATOR);
+            return files.get(files.size() - 1);
+        }
+
+        return latest;
     }
 }
--- rest/src/test/java/com/bwg/iot/AgentSoftwareUpgradeTests.java
+++ rest/src/test/java/com/bwg/iot/AgentSoftwareUpgradeTests.java
@@ -1,9 +1,5 @@
 package com.bwg.iot;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-
 import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,6 +11,10 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -41,21 +41,29 @@ public class AgentSoftwareUpgradeTests {
 
     @Test
     public void softwareVersionNotFound() throws Exception {
+        final File swUpgradeFolder = new File("./sw_upgrade");
+        swUpgradeFolder.mkdirs();
+        final File swUpgradePackage = new File(swUpgradeFolder, "gateway_agent.11.tar.gz");
+        IOUtils.copy(new ByteArrayInputStream(new byte[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1}), new FileOutputStream(swUpgradePackage));
+
         this.mockMvc
-                .perform(get("/sw_upgrade").param("currentBuildNumber", "not-existing"))
-                .andDo(print()).andExpect(status().is(204));
+                .perform(get("/sw_upgrade").param("currentBuildNumber", "9"))
+                .andDo(print()).andExpect(status().is(200)).andExpect(content().bytes(new byte[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1}));
+
+        swUpgradePackage.delete();
+        swUpgradeFolder.delete();
     }
 
     @Test
     public void softwareVersionFound() throws Exception {
         final File swUpgradeFolder = new File("./sw_upgrade");
         swUpgradeFolder.mkdirs();
-        final File swUpgradePackage = new File(swUpgradeFolder, "gateway_agent.existing.tar.gz");
-        IOUtils.copy(new ByteArrayInputStream(new byte[] {0,1,2,3,4,5,6,7,8,9,0}), new FileOutputStream(swUpgradePackage));
+        final File swUpgradePackage = new File(swUpgradeFolder, "gateway_agent.10.tar.gz");
+        IOUtils.copy(new ByteArrayInputStream(new byte[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0}), new FileOutputStream(swUpgradePackage));
 
         this.mockMvc
-                .perform(get("/sw_upgrade").param("currentBuildNumber", "existing"))
-                .andDo(print()).andExpect(status().is(200)).andExpect(content().bytes(new byte[]{0,1,2,3,4,5,6,7,8,9,0}));
+                .perform(get("/sw_upgrade").param("currentBuildNumber", "10"))
+                .andDo(print()).andExpect(status().is(204));
 
         swUpgradePackage.delete();
         swUpgradeFolder.delete();
